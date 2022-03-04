4f5673e285d8a8f02725f70f18ee070e0a461e05
Pradip Chaudhary <chaudharypradip678@gmail.com>
2022-03-04 17:03:18 +0545

    footer and header section design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,31 @@
 var changeClass = function(name) {
     $('#search, #nav ul').removeAttr('class').addClass(name);
 }
+
+$(document).ready(function() {
+
+    $("#testimonial-slider").owlCarousel({
+
+        items: 1,
+
+        itemsDesktop: [1000, 1],
+
+        itemsDesktopSmall: [979, 1],
+
+        itemsTablet: [768, 1],
+
+        pagination: true,
+
+        navigation: false,
+
+        slideSpeed: 1000,
+
+        singleItem: false,
+
+        navigationText: ["", ""],
+
+        autoPlay: true
+
+    });
+
+});
